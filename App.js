90e3db2be9e38bb7680414050a7277d37ad1932b
Ammar-e-Yasir <71455934+Ammar-e-Yasir@users.noreply.github.com>
2021-11-10 11:05:55 +0500

    Add files via upload

--- App.js
+++ App.js
@@ -1,13 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { View } from 'react-native';
 import RouteNavigation from './config/navigation';
 
 export default function App() {
   return (
-    <View>
       <RouteNavigation />
-    </View>
   )
 }
 
--- config/navigation.js
+++ config/navigation.js
@@ -18,7 +18,7 @@
 //     );
 // }
 import React from 'react';
-import { View, Text } from 'react-native'
+import { View, Text,StyleSheet } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
@@ -30,6 +30,7 @@ import Counter from '../screens/counter';
 import Home from '../screens/home';
 import About from '../screens/about';
 import { TouchableOpacity } from 'react-native-gesture-handler';
+import Contact from '../screens/contact';
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -53,20 +54,44 @@ function Food({navigation}){
 
 function SettingsScreen(){
     return (
-        <View>
-            <Text>Setting</Text>
+        <View style={{flex:1,justifyContent:'center',alignItems:'center',flexDirection:'column'}}>
+            <Text style={styles.view1}>Pakistan</Text>
+            <Text style={styles.view2}>Karachi</Text>
+            <Text style={styles.view3}>Lahore</Text>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    view1:{
+        backgroundColor:'red',
+        flex:1,
+        height:100,
+        width:200
+    },
+    view2:{
+        backgroundColor:'blue',
+        flex:2,
+        height:100,
+        width:200
+    },
+    view3:{
+        backgroundColor:'green',
+        flex:3,
+        height:100,
+        width:200
+    }
+})
 
 export default function RouteNavigation() {
     return (
         <NavigationContainer>
 
-            <Drawer.Navigator initialRouteName='food'>
+            <Drawer.Navigator initialRouteName='contact'>
                 <Drawer.Screen name='home' component={Home}  />
                 <Drawer.Screen name='settings' component={SettingsScreen} />
                 <Drawer.Screen name='food' component={Food} />
+                <Drawer.Screen name='contact' component={Contact} />
             </Drawer.Navigator>
             {/* {
                 false ? (
